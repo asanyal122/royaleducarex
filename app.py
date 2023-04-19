1784eca623401a4a84dfcebdab5fd8a63cb84b68
asanyal122 <asanyal122@gmail.com>
2023-04-19 19:53:10 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('applink.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/policy')
+def policy():
+    return render_template('policy.html')
+
+@app.route('/refund')
+def refund():
+    return render_template('refund.html')
+
+@app.route('/terms')
+def terms():
+    return render_template('terms.html')
